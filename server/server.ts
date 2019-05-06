fb2a47fefcfea6013b18b381a29f1378797ece7d
Miguel Aponte <miguelaponte2475@gmail.com>
2019-05-05 19:33:29 -0500

    Conexiòn a la BD

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mysql from 'mysql';
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
@@ -8,11 +9,27 @@ const server = express();
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
- 
+
 server.get('/', homeController.index);
 server.get('/users', homeController.users);
 server.post('/users/create', homeController.create);
 
-server.listen(3000, function () {
+server.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 });
+
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Password1·'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
